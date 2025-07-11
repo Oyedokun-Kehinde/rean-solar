12e42b8642a5164d0de4b249b9856fb63be58738
Oyedokun Kehinde <oyedokunken@gmail.com>
2025-07-11 20:32:47 +0100

    Update db.php

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
-$host = "localhost";  
-$username = "u153388584_admin";        
-$password = "ReanSolar100%";       
-$dbname = "u153388584_rean_solar"; 
+$host = "";  
+$username = "";        
+$password = "%";       
+$dbname = ""; 
 
 // Create connection
 $conn = new mysqli($host, $username, $password, $dbname);
